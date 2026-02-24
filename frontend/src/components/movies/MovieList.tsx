0c7ccb47983b5e19155f7243ee696c015c5620ba
Snirious <snir09@gmail.com>
2026-02-24 16:51:40 +0200

    create-trendingNow-ui

--- frontend/src/components/movies/MovieList.tsx
+++ frontend/src/components/movies/MovieList.tsx
@@ -1,4 +1,5 @@
 import type { MovieType } from "../../api/types/MoviesTypes";
+import { MovieCard } from "./MovieCard";
 
 type movieProps = {
   movies: MovieType[];
@@ -6,10 +7,12 @@ type movieProps = {
 
 export function MovieList({ movies }: movieProps) {
   return (
-    <div>
-      <ul>
+    <div className="relative">
+      <ul className="flex overflow-x-scroll overflow-y-visible space-x-4 px-4 md:px-6 py-4 scrollbar-hide relative">
         {movies.map((movie: MovieType) => (
-          <li key={movie.id}>{movie.name}</li>
+          <li key={movie.id}>
+            <MovieCard movie={movie} />
+          </li>
         ))}
       </ul>
     </div>
